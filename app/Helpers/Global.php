3cd060d2e568f0388797bd05b2104cd5c72b4639
Garrett <garrettcoleallen@gmail.com>
2025-01-15 10:19:59 -0500

    null coalesce org id lookup

--- app/Helpers/Global.php
+++ app/Helpers/Global.php
@@ -2,13 +2,13 @@
 
 use App\Models\Organizations\Organization;
 
-function current_organization_id() : int
+function current_organization_id() : ?int
 {
     // TODO - add a check for the "Context" current_organization_id
     return auth()->user()->current_organization_id;
 }
 
-function current_organization() : Organization
+function current_organization() : ?Organization
 {
     return Organization::find(current_organization_id());
 }
