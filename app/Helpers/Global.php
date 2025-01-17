90ae8c92c965c542f807cf5334ca35794261d747
Garrett <garrettcoleallen@gmail.com>
2025-01-17 14:31:10 -0500

    removed organization/team setting middleware updated PermissionRegistrar singleton to use override so we always use current_organization helper instead

--- app/Helpers/Global.php
+++ app/Helpers/Global.php
@@ -5,7 +5,7 @@ use App\Models\Organizations\Organization;
 function current_organization_id() : ?int
 {
     // TODO - add a check for the "Context" current_organization_id
-    return auth()->user()->current_organization_id;
+    return auth()->user()?->current_organization_id;
 }
 
 function current_organization() : ?Organization
