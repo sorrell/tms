3949a43d0a2d03127629193742d4eebaa477d750
Nick Sorrell <nicholassorrell@gmail.com>
2025-02-08 15:33:32 -0500

    Removing frontend render tests, pest will test backend

--- tests/Feature/Auth/EmailVerificationTest.php
+++ tests/Feature/Auth/EmailVerificationTest.php
@@ -5,13 +5,6 @@ use Illuminate\Auth\Events\Verified;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\URL;
 
-test('email verification screen can be rendered', function () {
-    $user = User::factory()->unverified()->create();
-
-    $response = $this->actingAs($user)->get('/verify-email');
-
-    $response->assertStatus(200);
-});
 
 // TODO: Fix this test when email verification is implemented as a feature/option
 // test('email can be verified', function () {
