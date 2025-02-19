5b95ca8c2009a0e30d897d2aaff4eca3b17233d5
Garrett <garrettcoleallen@gmail.com>
2025-02-19 13:44:28 -0500

    start of carrier lookup form

--- resources/js/Pages/Carriers/Index.tsx
+++ resources/js/Pages/Carriers/Index.tsx
@@ -14,32 +14,11 @@ import { Carrier } from '@/types';
 import { Head, router } from '@inertiajs/react';
 import { Plus } from 'lucide-react';
 import { useState } from 'react';
+import CarrierCreateDialog from './Partials/CarrierCreateDialog';
 
 export default function Index() {
     const [isOpen, setIsOpen] = useState(false);
-    const [carrierName, setCarrierName] = useState('');
-    const [errors, setErrors] = useState<Record<string, string>>({});
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        setErrors({});
-        router.post(
-            route('carriers.store'),
-            {
-                name: carrierName,
-            },
-            {
-                preserveScroll: true,
-                onSuccess: () => {
-                    setIsOpen(false);
-                    setCarrierName('');
-                },
-                onError: (errors) => {
-                    setErrors(errors);
-                },
-            },
-        );
-    };
 
     const setSelectedCarrier = (carrier: Carrier) => {
         router.visit(route('carriers.show', { carrier }));
@@ -60,7 +39,6 @@ export default function Index() {
                         type="button"
                         onClick={() => {
                             setIsOpen(true);
-                            setCarrierName('');
                         }}
                     >
                         <Plus /> Create Carrier
@@ -70,31 +48,7 @@ export default function Index() {
                     <CarrierList onSelect={setSelectedCarrier} />
                 </div>
             </div>
-            <Dialog open={isOpen} onOpenChange={setIsOpen}>
-                <DialogContent>
-                    <DialogHeader>
-                        <DialogTitle>Create Carrier</DialogTitle>
-                    </DialogHeader>
-                    <form onSubmit={handleSubmit} className="space-y-4">
-                        <Input
-                            placeholder="Carrier name"
-                            value={carrierName}
-                            onChange={(e) => setCarrierName(e.target.value)}
-                        />
-                        {errors.name && <InputError message={errors.name} />}
-                        <DialogFooter>
-                            <Button
-                                type="button"
-                                variant="outline"
-                                onClick={() => setIsOpen(false)}
-                            >
-                                Cancel
-                            </Button>
-                            <Button type="submit">Create</Button>
-                        </DialogFooter>
-                    </form>
-                </DialogContent>
-            </Dialog>
+            <CarrierCreateDialog isOpen={isOpen} setIsOpen={setIsOpen} />
         </AuthenticatedLayout>
     );
 }
---
+++ resources/js/Pages/Carriers/Partials/CarrierCreateDialog.tsx
@@ -0,0 +1,134 @@
+import { Input } from '@/Components/ui/input';
+
+import {
+    Dialog,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from '@/Components/ui/dialog';
+
+import InputError from '@/Components/InputError';
+import { Button } from '@/Components/ui/button';
+import { DialogContent } from '@/Components/ui/dialog';
+import { Label } from '@/Components/ui/label';
+import { router } from '@inertiajs/react';
+import { Search } from 'lucide-react';
+import { useState } from 'react';
+
+function CarrierManualCreateForm({
+    setIsOpen,
+}: {
+    setIsOpen: (isOpen: boolean) => void;
+}) {
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        setErrors({});
+        router.post(
+            route('carriers.store'),
+            {
+                name: carrierName,
+            },
+            {
+                preserveScroll: true,
+                onSuccess: () => {
+                    setIsOpen(false);
+                    setCarrierName('');
+                },
+                onError: (errors) => {
+                    setErrors(errors);
+                },
+            },
+        );
+    };
+
+    const [carrierName, setCarrierName] = useState('');
+    const [errors, setErrors] = useState<Record<string, string>>({});
+
+    return (
+        <form onSubmit={handleSubmit} className="space-y-4">
+            <Input
+                placeholder="Carrier name"
+                value={carrierName}
+                onChange={(e) => setCarrierName(e.target.value)}
+            />
+            {errors.name && <InputError message={errors.name} />}
+            <DialogFooter>
+                <Button
+                    type="button"
+                    variant="outline"
+                    onClick={() => setIsOpen(false)}
+                >
+                    Cancel
+                </Button>
+                <Button type="submit">Create</Button>
+            </DialogFooter>
+        </form>
+    );
+}
+
+function CarrierFmcsaCreateForm({
+    setIsOpen,
+    setFormState,
+}: {
+    setIsOpen: (isOpen: boolean) => void;
+    setFormState: (formState: 'manual' | 'fmcsa') => void;
+}) {
+    const handleSearch = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log('submitted');
+    };
+
+    return (
+        <form onSubmit={handleSearch} className="space-y-4">
+            <div className="space-y-2">
+                <Label>Carrier Name</Label>
+                <Input placeholder="Carrier name" />
+            </div>
+            <div className="space-y-2">
+                <Label>DOT Number</Label>
+                <Input placeholder="#######" />
+            </div>
+            <DialogFooter>
+                <Button
+                    type="button"
+                    variant="outline"
+                    onClick={() => setIsOpen(false)}
+                >
+                    Cancel
+                </Button>
+                <Button type="submit">
+                    <Search className="inline" /> Search
+                </Button>
+            </DialogFooter>
+        </form>
+    );
+}
+
+export default function CarrierCreateDialog({
+    isOpen,
+    setIsOpen,
+}: {
+    isOpen: boolean;
+    setIsOpen: (isOpen: boolean) => void;
+}) {
+    const [formState, setFormState] = useState<'manual' | 'fmcsa'>('fmcsa');
+
+    return (
+        <Dialog open={isOpen} onOpenChange={setIsOpen}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Create Carrier</DialogTitle>
+                </DialogHeader>
+                {formState === 'manual' && (
+                    <CarrierManualCreateForm setIsOpen={setIsOpen} />
+                )}
+                {formState === 'fmcsa' && (
+                    <CarrierFmcsaCreateForm
+                        setIsOpen={setIsOpen}
+                        setFormState={setFormState}
+                    />
+                )}
+            </DialogContent>
+        </Dialog>
+    );
+}
