65b59de4c828c03097b7f46aaf5137536328f809
Garrett <garrettcoleallen@gmail.com>
2025-03-19 15:44:28 -0400

    namespace fix

--- database/migrations/2025_03_19_183353_shipment_accounting_tables.php
+++ database/migrations/2025_03_19_183353_shipment_accounting_tables.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Currency;
+use App\Models\Accounting\Currency;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
